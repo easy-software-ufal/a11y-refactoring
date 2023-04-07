739faf29ba7629a3a755d934fa0a6703972a5e59
AnaFerreira015 <alsf@ic.ufal.br>
2023-04-07 17:54:27 -0300

    Allows more than one execution via command

--- extension.js
+++ extension.js
@@ -212,10 +212,10 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
+		accessOpenedFile();
 		vscode.window.showInformationMessage('a11y-refactoring is running!');
 	});
 
-	accessOpenedFile();
 	context.subscriptions.push(disposable);
 }
 
